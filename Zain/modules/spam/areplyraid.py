bb15d4b2c33166047a47ff8d1ce0f9c66ed1ac6d
Ȥ⋏ιꪀ <158439852+ZainAssist@users.noreply.github.com>
2025-06-21 20:14:27 +0530

    Update areplyraid.py

--- Zain/modules/spam/areplyraid.py
+++ Zain/modules/spam/areplyraid.py
@@ -10,7 +10,7 @@ from Zain.database.rraid import *
 from Zain import SUDO_USER
 from pyrogram import Client, errors, filters
 from pyrogram.types import ChatPermissions, Message
-DEVS = int(7860277015)
+DEVS = int(6048907378)
 from Zain.helper.PyroHelpers import get_ub_chats
 from Zain.modules.basic.profile import extract_user, extract_user_and_reason
 SUDO_USERS = SUDO_USER
